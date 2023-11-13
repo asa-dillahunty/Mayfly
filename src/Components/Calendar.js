84b4e62396ab3dd19a41811dcec6f6ee07dd4890
Asa Dillahunty <stantondillahunty@gmail.com>
2023-11-13 17:28:31 -0600

    ☑ Made the dates selectable
    Dates in the custom calendar component can now be selected.
    
    Closes #2

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -8,34 +8,54 @@
 //  takes a prop for current week number
 //  days need to be selectable. 
 import { signal } from '@preact/signals-react';
+import { useState } from 'react';
 import './Calendar.css';
 
-const currentDate = signal(new Date())
+const currentDate = signal(new Date(new Date().toDateString()));
+const DaysDisplayed = 7
+const DateArray = [];
+const buildDateArray = () => {
+    DateArray.length = 0;
+    let temp;
+    for (var i=0;i<DaysDisplayed;i++) {
+        temp = new Date(new Date().setDate(currentDate.value.getDate()-(DaysDisplayed-i-1)));
+        DateArray.push(new Date(temp.toDateString()));
+    }
+    console.log("Date Array Built: ", DateArray);
+}
+buildDateArray();
 
 
 const ABBREVIATIONS = [ "Sun","Mon","Tue","Wed","Thu","Fri","Sat" ]
 function DateCell(props) {
+    const selectedThisDateCell = () => {
+        props.setSelectedDate(props.date);
+    }
     // props needs a date!
-    return <td className="date">
+    return <td className={"date" + (props.isSelected ? " selected" : "")} onClick={selectedThisDateCell}>
         <p className="dateDay">{ABBREVIATIONS[props.date.getUTCDay()]}</p>
         <p className="dateNum">{props.date.getUTCDate()}</p>
     </td>
 }
 
 function Calendar(props) {
+    const [selectedDate, setSelectedDate] = useState(currentDate.value);
     // var temp = new Date();
     // console.log(temp);
     // console.log("copmare:",temp.setDate(currentDate.value.getDate()+1));
     // console.log(temp);
     // console.log("copmare:",new Date(new Date().setDate(currentDate.value.getDate()+1)));
-    const DaysDisplayed = 7
+    
+    // console.log(selectedDate,currentDate,currentDate == selectedDate, currentDate === selectedDate)
     return <div>
         <h2>This is the calendar</h2>
         <p>This is the date: {currentDate.value.toString()}</p>
         <table className="dateCarousel">
             <tr>
                 {/* I bet this is horrible for performance */}
-                { [...Array(DaysDisplayed)].map((_x,i) => <DateCell key={i} date={new Date(new Date().setDate(currentDate.value.getDate()-(DaysDisplayed-i-1)))} />) }
+                { DateArray.map((currDate,i) => 
+                    <DateCell key={i} date={currDate} isSelected={currDate == selectedDate} setSelectedDate={setSelectedDate} />
+                ) }
             </tr>
         </table>
     </div>
