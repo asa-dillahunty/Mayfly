717e8e67228c7c4bdb7fa8d2613745318d005504
Asa Dillahunty <stantondillahunty@gmail.com>
2023-12-27 12:33:06 -0600

    :construction: Pushing to test on phone
    Replaced hour picker with a wheel! It is a scroll area picker

--- src/Components/HourAdder.js
+++ src/Components/HourAdder.js
@@ -1,11 +1,26 @@
 import { hoursWorked } from "./firebase"
+import Picker from 'react-mobile-picker'
 import './HourAdder.css';
 
-
+const selections = {
+	hours: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
+	minutes: [0,.25,.5,.75],
+}
 
 function HourAdder(props) {
 	const handlePresetClick = (presetHours) => {
 		hoursWorked.value = presetHours;
+	};
+
+	const setHoursValue = (value) => {
+		// depending on what was changed, you only receive that
+		//	if you only change 'hours' only receive hours
+
+		if (value.hours)
+			hoursWorked.value = Number(value.hours + hoursWorked.value%1);
+		else
+			hoursWorked.value = Number(Math.floor(hoursWorked.value) + value.minutes)
+		// onClick={() => hoursWorked.value = (Number(hoursWorked) - 0.25)}
 	};
 
 	if (props.hide === true) return <div></div>
@@ -13,43 +28,20 @@ function HourAdder(props) {
 			<div className="hours-and-button-container">
 				<div className="worked-hours">
 					<p className="worked-hours-label">Hours Worked:</p>
-					<p>{hoursWorked}</p>
-				</div>
-				<div className="hours-controls">
-					<button
-						className="hours-button"
-						onClick={() => hoursWorked.value = (Number(hoursWorked) + 0.25)}
-					>
-						+
-					</button>
-					<button
-						className="hours-button"
-						onClick={() => hoursWorked.value = (Number(hoursWorked) - 0.25)}
-					>
-						-
-					</button>
+					<p>{hoursWorked.value}</p>
 				</div>
 			</div>
-			<div className="presets">
-				<button
-					className="preset-button"
-					onClick={() => handlePresetClick(4)}
-				>
-					4 hrs
-				</button>
-				<button
-					className="preset-button"
-					onClick={() => handlePresetClick(8)}
-				>
-					8 hrs
-				</button>
-				<button
-					className="preset-button"
-					onClick={() => handlePresetClick(12)}
-				>
-					12 hrs
-				</button>
-			</div>
+			<Picker height={50} value={hoursWorked.value} onChange={setHoursValue}>
+				{Object.keys(selections).map(name => (
+					<Picker.Column key={name} name={name}>
+					{selections[name].map(option => (
+						<Picker.Item key={option} value={option}>
+						{option}
+						</Picker.Item>
+					))}
+					</Picker.Column>
+				))}
+			</Picker>
 			<button onClick={props.handleAddHours}>Add Hours</button>
 		</div>
 }
